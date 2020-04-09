92d8d7c804df7b5409585592889b594bbe88996d
Shubham Goel <46935494+shubhamgoel2525@users.noreply.github.com>
2020-04-10 00:54:17 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -60,13 +60,8 @@ function appendData(data) {
             <p class="text-sm md:text-lg text-gray-600 flex text-center items-center">
                 Pay via: &nbsp; 
                 ${PayFN(match.paymentModes)}
-                <!-- <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-    <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-</svg> -->
-                <!-- Insert different svg graphics like cash for COD, paytm , atm for online banking etc. -->
+                ${showMenu(match.dishes)}
             </p>
-            
-            ${showMenu(match.dishes)}
         </div>
       </div>
         `).join('');
